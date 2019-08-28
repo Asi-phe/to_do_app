f583b1a0e45b7cd31f7c6085032fc8790fb058ba
Asi-phe <mazamisa.zisu@gmail.com>
2019-08-28 14:19:52 +0200

    anding a js link

--- index.php
+++ index.php
@@ -1,68 +1,28 @@
-
-<!--Creating a form and a condition that displays the output of the form-->
-?>
-   <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" id="quiz">
-   <?php foreach ($Questions as $QuestionNo => $Value){ ?>
-       <h3><?php echo $Value['Question']; ?></h3>
-       <?php
-           foreach ($Value['Answers'] as $Letter => $Answer){
-           $Label = 'question-'.$QuestionNo.'-answers-'.$Letter;
-           echo '<br />';
-       ?>
-         <div>
-           <input type="radio" name="answers[<?php echo $QuestionNo; ?>]" id="<?php echo $Label; ?>" value="<?php echo $Letter; ?>" />
-           <label for="<?php echo $Label; ?>"><?php echo $Letter; ?>) <?php echo $Answer; ?> </label>
-       </div>
-       <?php } ?>
-   <?php } ?>
-   <?php }
-    
-   echo $Answers ?>
-   <input id="button" type="submit" value="Submit Quiz" />
-   </form>
 <?php
-if($counter){
-}
-?>
-
-Today
-Asiphe Mazamisa 12:55 PM
-<?php
-//starting the session with the session start function
 session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" type="text/css" href="css/style.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <title>To do list</title>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <link rel="stylesheet" type="text/css" href="css/style2.css">
+   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro&display=swap" rel="stylesheet">
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+   <title>To do list</title>
 </head>
 <body>
-  <!-- <div class="list">
-      <h1 class="header">Welcome to my to do list:</h1>
-      <ul class="items">
-          <li>
-              <span class="item">Study for exams</span>
-          </li>
-          <li>
-              <span class="item done">Cook for supper</span>
-          </li>
-      </ul> -->
-      <!-- class="input" placeholder="add item in the to do list" autocomplete="off " required> -->
-      <form action="index.php" method="post">
-          <input type="text" name="todoEntry" >
-          <button type="submit" >Add</button>
-      </form>
-  <!-- </div> -->
-  <?php
+       <h1>My to do list is as follows</h1>
+       <form action="index2.php" method="post">
+           <input type="text" name="todoEntry" >
+           <button type="submit" >Add</button>
+       </form>
+   <?php
 if($_POST){
-  echo "<br>"."\n<ul>\n<li>".$_POST['todoEntry']."</li>\n</ul>";
+   echo "<br>"."\n<ul>\n<li>".$_POST['todoEntry']."</li>\n</ul>";
 }
-  ?>
+   ?>
 <script src="js/main.js"></script>
 </body>
 </html>
--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
-[11:32 AM]
+
+
+$(document).ready(
+  function(){
+      $('#button').click(
+          function(){
+              var toAdd = $('input[name=toDoEntry]').val();
+               $('ul').append('<li>' + toAdd + '</li>');
+          });
 
 $(document).ready(function(){
     //craeting a variable called itemDone and initializing it to zero.
@@ -8,12 +16,12 @@ $(document).ready(function(){
       if(itemDone == 0){
           $(this).css("text-decoration","line-through");
           itemDone = 1;
-          console.log(itemDone);
+          consule.log(itemDone);
       }else{
           //if the variable is selected,let the line decoration not strike through
       $(this).css("text-decoration","none");
       itemDone = 0;
-      console.log(itemDone);
+      consule.log(itemDone);
       }
   });
 });
