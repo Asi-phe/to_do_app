c191499b079de21e63f97f35ede1e01be2021bae
Asi-phe <mazamisa.zisu@gmail.com>
2019-08-28 16:59:22 +0200

    background image gradient

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-//session_destroy();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,41 +7,29 @@ session_start();
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-   <!-- bootsrapp link -->
-   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-   integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-   <link rel="stylesheet" href="css/style.css">
-   <title>to do App</title>
+   <link rel="stylesheet" type="text/css" href="css/style.css">
+   <!--Linking google fonts-->
+   <link href="https://fonts.googleapis.com/css?family=Lexend+Mega&display=swap" rel="stylesheet">
+   <!--Linking JQuery-->
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+   <title>To do list</title>
 </head>
 <body>
-</form class="todo" >
-<h1>To do list</h1>
-<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>"method="post">
-<input required type="text" name="item">
-<input type="submit" value="ADD">
-</form>
 
-<?php
-if(isset($_POST)){
-   $any = $_POST['item'];
-   if(isset($_SESSION['items'])){
-       //if isset have been set let the value of $_SESSION be equal to $any
-       $_SESSION['items'][] = $any;
-   }else{
-       // else let $_SESSION be equal to an empty array
-       $_SESSION['items'] = [];
-   }
-
-
-
-    echo "<ul>";
-    foreach($_SESSION['items'] as $item){
-        echo "<li>".$item."   "."</li><br>";
-    };
-    echo "</ul>";
-  }
-?>
+       <h1>To do list</h1>
+       <form action="index.php" method="post">
+           <input type="text" name="todoEntry" >
+           <button type="submit" >Add</button>
+       </form>
+   <?php
+if($_POST){
+   echo "<br>"."\n<ul>\n<li>".$_POST['todoEntry']."</li>\n</ul>";
+}
+   ?>
+<script src="js/main.js"></script>
 </body>
 </html>
+
+
 
 
